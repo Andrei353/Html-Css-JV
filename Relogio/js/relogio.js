87dae8a1ea7126272b58cd40d72aa3deb30c4ad2
Andrei353 <143633137+Andrei353@users.noreply.github.com>
2024-03-15 16:26:33 -0300

    Update relogio.js

--- Relogio/js/relogio.js
+++ Relogio/js/relogio.js
@@ -3,12 +3,15 @@
 const horas = document.querySelector('#horas')
 const minutos = document.querySelector('#minutos')
 const segundos = document.querySelector('#segundos')
-
+const data = document.querySelector('#dia')
+const mes = document.querySelector('#mes')
+const ano = document.querySelector('#ano')
+const frase = document.querySelector('#frase')
+const alvo = document.querySelector('img')
 
 //eventos
 
 setInterval(relogio,1000)
-
 
 
 //funções
@@ -18,6 +21,10 @@ function relogio(){
     let hr = dia.getHours()
     let min = dia.getMinutes()
     let seg = dia.getSeconds()
+    let dt = dia.getDate()
+    let mm = dia.getMonth()+1
+    let aa = dia.getFullYear()
+
 
     if(hr<10){
         hr="0"+hr
@@ -31,8 +38,33 @@ function relogio(){
         seg="0"+seg
     }
 
+    if(dt<10){
+       dt="0"+dt
+    }
+
+    if(mm<10){
+        mm="0"+mm
+    }
+
+//---------------------------------
+
+     if(hr>=5 && hr<12){
+       frase.textContent = 'Bom dia'
+       alvo.src = 'image/sun_3073665.png'
+     }else if(hr>=12 && hr<18){
+        frase.textContent = 'Boa tarde'
+        alvo.src = 'image/sun_3073665.png'
+     }else{
+        frase.textContent = 'Boa noite'
+        alvo.src = 'image/lua.png'
+     }    
+
+//---------------------------------
     horas.textContent = hr
     minutos.textContent = min
     segundos.textContent = seg
+    data.textContent = dt
+    mes.textContent = mm
+    ano.textContent = aa
 
 }
