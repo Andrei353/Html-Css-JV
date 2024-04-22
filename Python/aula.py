78af82e37edd0d1c86b535e7a296682e53fb44bf
Andrei353 <143633137+Andrei353@users.noreply.github.com>
2024-04-22 16:23:36 -0300

    Add files via upload

---
+++ Python/aula.py
@@ -0,0 +1,69 @@
+#print serve para imprimir na tela"""
+#a = 3;
+# = 4;
+
+#oma = a+b;
+
+#print("A soma de a + b é igual a:", soma);
+
+#Operação matematica print(2+2)Adição ,print(4-2)Subtração ,print(2*2)Mutiplicação ,print(10/2)Divisão ,print(2**3)Exponenciação
+
+"""
+idVendedor=int (input("Digite o seu Id:"));
+os.system("cls");
+codPeca=int (input("Informe  o código da peça:"));
+os.system("cls");
+precoUnit=float (input("Informe o preço:"));
+os.system("cls");
+qtd=float (input("Informe a quantidade:"));
+
+valor= qtd*precoUnit;
+comissao= (valor*5)/100;
+
+print("A comissão do vendedor é:", comissao);
+"""
+"""
+x = 10
+
+
+if x>5:
+    print("x é maior que 5");
+else:
+    print("x é menor que 5");
+
+print("fim");
+"""
+"""
+senhaCorreta = (input("Digite sua Senha de Acesso:"));
+os.system("cls");
+
+
+if senhaCorreta == senha:
+    print("Acesso Permitido");
+else:
+    print("Senha Incorreta(VC É BURRO O SUFICIENTE PRA ESQUECER SUA SENHA)"); 
+"""
+
+
+
+
+
+import os
+cores= (input("Digite a cor do CD que deseja:"));
+os.system("cls");
+
+
+if cores == "Verde":
+    print("O CD custa R$10,00");
+elif cores == "Azul":
+    print("O CD custa R$20,00");
+elif cores == "Amarelo":
+    print("O CD custa R$30,00");
+elif cores == "Vermelho":
+    print("O CD custa R$40,00")    
+else:
+    print("Cor ñ encontrada pfvr tente outra hora...(Burro pra carai, tem um placa gigante la fora mostrando as cores))");            
+
+
+
+
